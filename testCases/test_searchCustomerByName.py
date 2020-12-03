1f88020e652615c7fe2715ec4b0c10e32d12c65e
Arfa <aowtearfa@gmail.com>
2020-12-03 20:52:41 +0530

    This is my first commit .....

---
+++ testCases/test_searchCustomerByName.py
@@ -0,0 +1,50 @@
+import time
+import pytest
+from pageObjects.LoginPage import LoginPage
+from pageObjects.AddCustomerPage import AddCustomer
+from pageObjects.SearchCustomerPage import SearchCustomer
+from utilities.readProperties import ReadConfig
+from utilities.customLogger import LogGen
+
+class Test_SearchCustomerByName_005:
+    baseURL = ReadConfig.getApplicationURL()
+    username = ReadConfig.getUseremail()
+    password = ReadConfig.getPassword()
+    logger = LogGen.loggen()  # Logger
+
+    @pytest.mark.regression
+    def test_searchCustomerByName(self,setup):
+        self.logger.info("************* SearchCustomerByName_005 **********")
+        self.driver=setup
+        self.driver.get(self.baseURL)
+        self.driver.maximize_window()
+
+        self.lp = LoginPage(self.driver)
+        self.lp.setUserName(self.username)
+        self.lp.setUserPassword(self.password)
+        self.lp.clickLogin()
+        self.logger.info("************* Login succesful **********")
+
+        self.logger.info("******* Starting Search Customer By Name **********")
+
+        time.sleep(5)
+        self.addcust = AddCustomer(self.driver)
+
+        time.sleep(5)
+        self.addcust.clickOnCustomersMenu()
+
+        time.sleep(5)
+        self.addcust.clickOnCustomersMenuItem()
+        time.sleep(2)
+
+        self.logger.info("************* searching customer by Name **********")
+        searchcust = SearchCustomer(self.driver)
+        searchcust.setFirstName("Victoria")
+        searchcust.setLastName("Terces")
+        time.sleep(5)
+        searchcust.clickSearch()
+        time.sleep(5)
+        status=searchcust.searchCustomerByName("Victoria Terces")
+        self.driver.close()
+        assert True==status
+        self.logger.info("***************  TC_SearchCustomerByName_005 Finished  *********** ")
